6127d35fe4c61f5e7d8a73fa923abf7cf62a00ff
“Juan <shael2540@gmail.com>
2023-08-02 23:58:29 +0200

    proyecto final

---
+++ data/stock.js
@@ -0,0 +1,94 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Discos 25KG",
+        tipo: "Disco",
+        desc: "Par de disco ROUGE de alta calidad",
+        precio:  260.15 ,
+        imagen: '../public/images/disco25.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Discos 20KG",
+        tipo: "Disco",
+        desc: "Par de disco ROUGE de alta calidad",
+        precio:  + 211.75 ,
+        imagen: '../public/images/disco20.png',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Discos 15KG",
+        tipo: "Disco",
+        desc: "Par de disco ROUGE de alta calidad",
+        precio:  163.35,
+        imagen: '../public/images/disco15.png',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Discos 10KG",
+        tipo: "Disco",
+        desc: "Par de disco ROUGE de alta calidad",
+        precio:  118.58,
+        imagen: '../public/images/disco10.png',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Discos 5KG",
+        tipo: "Disco",
+        desc: "Par de disco ROUGE de alta calidad",
+        precio:  90.75,
+        imagen: '../public/images/disco5.png',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Discos 2,5KG ",
+        tipo: "Disco",
+        desc: "Par de disco ROUGE de alta calidad",
+        precio: 66.55,
+        imagen: '../public/images/discoDosYmedio.png',
+        cantidad:1
+    },
+
+    {
+        id: 7,
+        nombre: "Discos 1,25KG",
+        tipo: "Disco",
+        desc: "Par de disco ROUGE de alta calidad",
+        precio:  48.40,
+        imagen: '../public/images/disco125.png',
+        cantidad:1
+    },
+
+    {
+        id: 8,
+        nombre: "Discos 0.5KG",
+        tipo: "Disco",
+        desc: "Par de disco ROUGE de alta calidad",
+        precio:  37.60,
+        imagen: '../public/images/disco05.png',
+        cantidad:1
+    },
+
+    {
+        id: 9,
+        nombre: "Discos 0,25KG",
+        tipo: "Disco",
+        desc: "Par de discos ROUGE de alta calidad",
+        precio:  30.25,
+        imagen: '../public/images/disco025.png',
+        cantidad:1
+    },
+
+
+];
