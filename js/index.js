cc0266c18ebe905458280184cc164883b52ef0d4
“Juan <shael2540@gmail.com>
2023-09-03 14:03:24 +0200

    correccion

--- js/index.js
+++ js/index.js
@@ -1,18 +1,11 @@
-document.addEventListener('DOMContentLoaded', pintarProductos(productos));
+
 document.addEventListener('DOMContentLoaded', cargarCarrito(carrito));
 
-// document.addEventListener("DOMContentLoaded", () => {
-// 	fetch("https://github.com/shael86/repositorio.js.git")
-// 		.then((response) => response.json())
-// 		.then((productos) => {
-// 			pintarProductos(productos);
-// 		});
-// });
-// document.addEventListener("DOMContentLoaded", cargarCarrito());
-
-// window.onload = async () => {
-//     await productos();
-//     pintarProducto();
-//     pintarProductoCarrito();
-//     carrito();
-// };
+document.addEventListener("DOMContentLoaded", () => {
+console.log('test');
+	fetch("/data/stock.json")
+		.then((response) => response.json())
+		.then((productos) => {
+			pintarProductos(productos);
+		});
+});
